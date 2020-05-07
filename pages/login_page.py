feb5bede76babdb6df7b2ea4840b56f984c1c59b
Denzuy <den-zuy@mail.ru>
2020-05-07 22:39:55 +0300

    Added Locators and Login Page
    Added files Locators only for Selenium locators and Login Page only for Login Page methods

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Current URL is not have word login in URL"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+        "Login from is not found"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+        "Register from is not found"
