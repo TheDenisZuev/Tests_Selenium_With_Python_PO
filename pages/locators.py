0e095155f934c38873bb9a6687b7395e65bd9da0
Denzuy <den-zuy@mail.ru>
2020-05-10 17:46:08 +0300

    Added product page Test
    Added new test fot Product Page: user added book in cart, we check name book and price book

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,15 @@
 from selenium.webdriver.common.by import By
 
-
 class MainPageLocators():
     LOGIN_LINK = (By.XPATH, "//a[@id='login_link']")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.XPATH, "//form[@id='login_form']")
     REGISTER_FORM = (By.XPATH, "//form[@id='register_form']")
+
+class ProductPageLocators():
+    ADD_CART_BUTTON = (By.XPATH, "//form[@id='add_to_basket_form']//button[@type='submit']")
+    BOOK_NAME = (By.XPATH, "//h1")
+    BOOK_PRICE = (By.XPATH, "//div[@class='col-sm-6 product_main']//p[@class='price_color']")
+    CART_TOTAL_PRICE = (By.XPATH, "//div[@class='alertinner ']//p//strong")
+    ADDED_BOOK_NAME = (By.XPATH, "(//div[@id='messages']//strong)[1]")
---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def should_be_match_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        added_book_name = self.browser.find_element(*ProductPageLocators.ADDED_BOOK_NAME).text
+        assert book_name == added_book_name, f"Added book is not equal book. Our book {book_name} " \
+                                             f"Actual result {added_book_name}"
+    def should_be_match_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        cart_total_price = self.browser.find_element(*ProductPageLocators.CART_TOTAL_PRICE).text
+        assert book_price == cart_total_price, f"Added book price is not equal count in cart. Our price {book_price} " \
+                                             f"Actual result {cart_total_price}"
+
